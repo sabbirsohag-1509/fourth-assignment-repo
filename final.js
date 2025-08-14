3919eed77bfc9448c7816f21dc75c5d948f32ef7
sabbirsohag-1509 <sabbirhossainsohag1509@gmail.com>
2025-08-14 10:44:46 +0600

    NaN correction

--- final.js
+++ final.js
@@ -76,6 +76,9 @@ function  resultReport( marks ) {
         if(!Array.isArray(marks)){
             return 'Invalid';
         }
+        if(marks.length === 0){
+            return {finalScore: 0, pass: 0, fail: 0};
+        }
         let sum = 0;
         let passCount = 0;
         let failCount = 0;
@@ -95,3 +98,4 @@ function  resultReport( marks ) {
 
         return{ finalScore:roundAvgMarks, pass:passCount, fail:failCount};
 }
+  
--- problem-5.js
+++ problem-5.js
@@ -3,6 +3,9 @@ function  resultReport( marks ) {
         if(!Array.isArray(marks)){
             return 'Invalid';
         }
+        if(marks.length === 0){
+            return {finalScore: 0, pass: 0, fail: 0};
+        }
         let sum = 0;
         let passCount = 0;
         let failCount = 0;
@@ -23,5 +26,5 @@ function  resultReport( marks ) {
         return{ finalScore:roundAvgMarks, pass:passCount, fail:failCount};
 }
 
-const input = 100;
+const input =100;
 console.log(resultReport(input));
